4fafbbfdd7b126663d78cc05684f60350e4491ac
SunnyFlops <arav.prasad@outlook.com>
2026-05-01 12:33:30 +1200

    new layout design + placeholders

---
+++ index.js
@@ -0,0 +1,37 @@
+// date UI
+const dateText = document.getElementById("dateTimeUI");
+
+function updateTimeText() {
+    const date = new Date();
+
+    dateText.innerText =
+        date.toLocaleDateString("en-US", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+        })
+        + " • " +
+        date.toLocaleTimeString("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+}
+
+updateTimeText();
+setInterval(updateTimeText, 3000);
+
+// name editor
+const nameText = document.getElementById("schemaName");
+
+nameText.addEventListener("click", () => {
+    nameText.setAttribute("contenteditable", "true");
+    nameText.focus();
+});
+
+nameText.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        nameText.removeAttribute("contenteditable");
+        nameText.blur();
+    }
+});
