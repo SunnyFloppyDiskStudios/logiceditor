80d7b79f05e34c210ed42efc7164a622c205387f
SunnyFlops <arav.prasad@outlook.com>
2026-05-01 20:25:39 +1200

    planning gem

---
+++ objects/core.js
@@ -0,0 +1,8 @@
+/*
+notes space
+
+script that manages all the connections
+
+each "node" gets a random UUID i guess and allat connects together but inside each unique script the uuids are linked towards inputs/outputs
+
+ */
