87dffeafb6a2259cfbabea38ac214e45fead7c60
SunnyFlops <arav.prasad@outlook.com>
2026-05-07 15:12:11 +1200

    make clickzones clickable + wire boilerplate

--- objects/core.js
+++ objects/core.js
@@ -6,3 +6,13 @@ script that manages all the connections
 each "node" gets a random UUID i guess and allat connects together but inside each unique script the uuids are linked towards inputs/outputs
 
  */
+
+var clickZones = document.getElementsByClassName("wire-clickzone");
+
+function createWire(e) {
+    console.log(e.currentTarget);
+}
+
+for (var i = 0; i < clickZones.length; i++) {
+    clickZones[i].addEventListener('click', createWire, false);
+}
